c251b8ef143d0d0b7d361df048738782257eee47
Siddiqov Samandar <samandarsiddiqov31@gmail.com>
2025-10-31 18:05:36 +0500

    Completed

--- book/settings.py
+++ book/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4kl&!l3x!fadl+hvz2)gx-$7cyl1z7qlpsu0v!r#-x(k(vr8pd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://samandars10.pythonanywhere.com/",]
+ALLOWED_HOSTS = ["https://samandars10.pythonanywhere.com/", '127.0.0.1', 'localhost']
 
 
 # Application definition
